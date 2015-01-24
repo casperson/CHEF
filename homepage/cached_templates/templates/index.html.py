e7db4e60b3dfc818f5078d9521e9fcef506cddaf
Braden Casperson <bradencasperson@gmail.com>
2015-01-24 13:53:47 -0700

    Updated template

--- homepage/cached_templates/templates/index.html.py
+++ homepage/cached_templates/templates/index.html.py
@@ -4,13 +4,13 @@ UNDEFINED = runtime.UNDEFINED
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1421802613.892322
+_modified_time = 1422132466.91195
 _enable_loop = True
 _template_filename = 'C:\\Users\\Braden\\chef\\homepage\\templates/index.html'
 _template_uri = 'index.html'
 _source_encoding = 'ascii'
 import os, os.path, re
-_exports = ['content']
+_exports = ['content_center']
 
 
 def _mako_get_namespace(context, name):
@@ -28,12 +28,12 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        def content():
-            return render_content(context._locals(__M_locals))
+        def content_center():
+            return render_content_center(context._locals(__M_locals))
         __M_writer = context.writer()
         __M_writer('\r\n\r\n')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
-            context['self'].content(**pageargs)
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content_center'):
+            context['self'].content_center(**pageargs)
         
 
         return ''
@@ -41,13 +41,13 @@ def render_body(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
-def render_content(context,**pageargs):
+def render_content_center(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        def content():
-            return render_content(context)
+        def content_center():
+            return render_content_center(context)
         __M_writer = context.writer()
-        __M_writer('\r\n    <div class="content">\r\n      <h3>Congratulations -- you\'ve successfully created a new django-mako-plus app!</h3>\r\n      <h4>Next Up: Go through the django-mako-plus tutorial and add Javascript, CSS, and urlparams to this page.</h4>\r\n    </div>\r\n')
+        __M_writer('\r\n    <div class="container-fluid">\r\n        <div class="col-xs-2"></div>\r\n        <div class="col-xs-8" style="margin-top: 10px"><img src="/static/homepage/media/colonial_flag.jpg" height="400"></div>\r\n        <div class="col-xs-2"></div>\r\n    </div>\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -55,6 +55,6 @@ def render_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"uri": "index.html", "source_encoding": "ascii", "line_map": {"56": 50, "34": 1, "27": 0, "44": 3, "50": 3}, "filename": "C:\\Users\\Braden\\chef\\homepage\\templates/index.html"}
+{"source_encoding": "ascii", "uri": "index.html", "filename": "C:\\Users\\Braden\\chef\\homepage\\templates/index.html", "line_map": {"56": 50, "34": 1, "27": 0, "44": 3, "50": 3}}
 __M_END_METADATA
 """

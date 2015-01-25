cc4b45cd526b1034e4d80eb5d1e113daeace8c60
relateteam <garrett@relate-institute.org>
2015-01-24 20:33:16 -0700

    Added modal to inventory

--- homepage/cached_templates/templates/event.html.py
+++ homepage/cached_templates/templates/event.html.py
@@ -4,7 +4,7 @@ UNDEFINED = runtime.UNDEFINED
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1422148229.026069
+_modified_time = 1422149577.481725
 _enable_loop = True
 _template_filename = 'C:\\Users\\Garrett\\PycharmProjects\\CHEF\\homepage\\templates/event.html'
 _template_uri = 'event.html'
@@ -47,7 +47,7 @@ def render_content_center(context,**pageargs):
         def content_center():
             return render_content_center(context)
         __M_writer = context.writer()
-        __M_writer('\r\n<h1>Events</h1>\r\n    <div style="margin-top: 10px" class="container-fluid">\r\n\r\n        <div style="margin-top: 10px">\r\n\r\n           <button style="margin-bottom: 15px" type="submit" class="btn btn-success">Add Event</button>\r\n           <table class="table table-striped">\r\n                <tr>\r\n                    <th>Event Name</th>\r\n                    <th>Location</th>\r\n                    <th>Event Area</th>\r\n                    <th>Description</th>\r\n                    <th>Start Date</th>\r\n                    <th>End Date</th>\r\n                </tr>\r\n                <tr>\r\n                    <td>Colonial Festival</td>\r\n                    <td>Orem, UT</td>\r\n                    <td>Area 3</td>\r\n                    <td>The Foundation sponsors the Colonial Heritage Festival, the largest colonial living and reenactment event west of the Mississippi River.</td>\r\n                    <td>July 4th</td>\r\n                    <td>July 5th</td>\r\n                </tr>\r\n           </table>\r\n\r\n        </div>\r\n\r\n    </div>\r\n')
+        __M_writer('\r\n<h2 style="text-decoration: underline">Events</h2>\r\n    <div style="margin-top: 10px" class="container-fluid">\r\n\r\n        <div style="margin-top: 10px">\r\n\r\n           <button style="margin-bottom: 15px" type="submit" class="btn btn-success">Add Event</button>\r\n           <table class="table table-striped" style="background-color: white; border: .5px solid #DDDDDD">\r\n                <tr>\r\n                    <th>Event Name</th>\r\n                    <th>Location</th>\r\n                    <th>Event Area</th>\r\n                    <th>Description</th>\r\n                    <th>Start Date</th>\r\n                    <th>End Date</th>\r\n                    <th>Edit/Delete</th>\r\n                </tr>\r\n                <tr>\r\n                    <td>Colonial Festival</td>\r\n                    <td>Orem, UT</td>\r\n                    <td>Area 3</td>\r\n                    <td>The Foundation sponsors the Colonial Heritage Festival, the largest colonial living and reenactment event west of the Mississippi River.</td>\r\n                    <td>July 4th</td>\r\n                    <td>July 5th</td>\r\n                    <td>\r\n                        <div class="btn-group" role="group" aria-label="...">\r\n                            <button type="button" class="btn btn-default"><i class="fa fa-edit"></i></button>\r\n                            <button type="button" class="btn btn-danger"><i class="fa fa-trash"></i></button>\r\n                        </div>\r\n                    </td>\r\n                </tr>\r\n           </table>\r\n\r\n        </div>\r\n\r\n    </div>\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
